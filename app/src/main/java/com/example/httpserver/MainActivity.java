34182f5eeef5939a6a81b0e257dbefa92fb68227
Zhursat <163448543+Zhursat@users.noreply.github.com>
2026-03-18 07:15:14 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/httpserver/MainActivity.java
+++ app/src/main/java/com/example/httpserver/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        startService(new Intent(this, ServerService.class));
         dbHelper = new DBHelper(this);
 
         LinearLayout layout = new LinearLayout(this);
