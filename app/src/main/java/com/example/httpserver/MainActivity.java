578a49cfdc827e208a64d65d1795ce4683100a38
Zhursat <163448543+Zhursat@users.noreply.github.com>
2026-03-18 17:48:14 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/httpserver/MainActivity.java
+++ app/src/main/java/com/example/httpserver/MainActivity.java
@@ -1,1 +1,16 @@
-startService(new Intent(this, ServerService.class));
+package com.example.httpserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 🔥 запуск сервера
+        startService(new Intent(this, ServerService.class));
+    }
+}
