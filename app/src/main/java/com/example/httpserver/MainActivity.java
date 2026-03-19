1906338a9662e5d8e4a2f9655db309b163a104a8
Zhursat <163448543+Zhursat@users.noreply.github.com>
2026-03-19 13:52:33 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/httpserver/MainActivity.java
+++ app/src/main/java/com/example/httpserver/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.httpserver;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +11,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // UI
+        setContentView(R.layout.activity_main);
+
         // 🔥 запуск сервера
         startService(new Intent(this, ServerService.class));
+
+        // кнопка переходу в ControlActivity
+        findViewById(R.id.btnControl).setOnClickListener(v -> {
+            startActivity(new Intent(this, ControlActivity.class));
+        });
     }
 }
